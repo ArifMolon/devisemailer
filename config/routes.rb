ba6a2e0d71461c0d399dd8009ee212a03d0a6180
Arif Molon <arifmolon@gmail.com>
2016-12-25 22:09:23 +0000

    scaffole articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
+  
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'pages#index'
-
+  resources :articles
   get 'pages/contact'
-
   get 'pages/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
